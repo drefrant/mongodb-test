def4f8b3507c2c45080d82d66d56a61551720336
drefrant <diensa.refranto@gmail.com>
2017-11-02 22:14:48 +1000

    update test folder with mocha

---
+++ test/saving_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+// Describe tests
+describe('Saving records', function(){
+
+    // Create tests
+    it('Save a record to the database', function(done){
+        
+        var char = new MarioChar({
+            name: 'Mario'
+        });
+
+        char.save().then(function(){
+            assert(char.isNew === false);
+            done();
+        });
+
+    });
+});
